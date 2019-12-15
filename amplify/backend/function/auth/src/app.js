87a50ed324830f60f84952e3ce303564942f23a3
Prashanth HN <prashanth@Prashanths-MacBook-Pro.local>
2019-12-15 15:30:43 +0530

    generate & validate OTP

--- amplify/backend/function/auth/src/app.js
+++ amplify/backend/function/auth/src/app.js
@@ -1,11 +1,4 @@
-/* Amplify Params - DO NOT EDIT
-You can access the following resource attributes as environment variables from your Lambda function
-var environment = process.env.ENV
-var region = process.env.REGION
-var storageUsersName = process.env.STORAGE_USERS_NAME
-var storageUsersArn = process.env.STORAGE_USERS_ARN
-
-Amplify Params - DO NOT EDIT *//*
+/*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
     http://aws.amazon.com/apache2.0/
@@ -14,7 +7,18 @@ See the License for the specific language governing permissions and limitations 
 */
 
 
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+var storageAuthusersName = process.env.STORAGE_AUTHUSERS_NAME
+var storageAuthusersArn = process.env.STORAGE_AUTHUSERS_ARN
 
+Amplify Params - DO NOT EDIT */
+
+const AWS = require('aws-sdk')
+AWS.config.update({ region: 'ap-south-1' });
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 var express = require('express')
 var bodyParser = require('body-parser')
@@ -26,7 +30,7 @@ app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
@@ -37,61 +41,104 @@ app.use(function(req, res, next) {
  * Example get method *
  **********************/
 
-app.get('/auth', function(req, res) {
+app.get('/auth', function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  res.json({ success: 'get call succeed!', url: req.url });
 });
 
-app.get('/auth/*', function(req, res) {
+app.get('/auth/*', function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  res.json({ success: 'get call succeed!', url: req.url });
 });
 
 /****************************
 * Example post method *
 ****************************/
 
-app.post('/auth', function(req, res) {
+app.post('/auth', async function (req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  const otp = Math.floor(100000 + Math.random() * 900000)
+  const item = { phone: req.body.phone, otp }
+  let putItemParams = {
+    TableName: 'authusers-dev',
+    Item: item
+  }
+  await dynamodb.put(putItemParams, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.json({ error: err, url: req.url, body: req.body });
+    } else {
+      res.json({ success: 'put call succeed!', url: req.url, data: data })
+    }
+  });
 });
 
-app.post('/auth/*', function(req, res) {
+app.post('/auth/verify', async function (req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  console.log(req.body)
+  let getItemParams = {
+    TableName: "authusers-dev",
+    Key: { phone: req.body.phone }
+  }
+
+  const user = await getItem(getItemParams)
+  console.log(user)
+  if (user.otp === parseInt(req.body.otp)) {
+    res.json({ success: 'Valid OTP' })
+  } else {
+    res.json({ success: 'Invalid OTP' })
+  }
 });
 
 /****************************
 * Example put method *
 ****************************/
 
-app.put('/auth', function(req, res) {
+app.put('/auth', function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({ success: 'put call succeed!', url: req.url, body: req.body })
 });
 
-app.put('/auth/*', function(req, res) {
+app.put('/auth/*', function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({ success: 'put call succeed!', url: req.url, body: req.body })
 });
 
 /****************************
 * Example delete method *
 ****************************/
 
-app.delete('/auth', function(req, res) {
+app.delete('/auth', function (req, res) {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  res.json({ success: 'delete call succeed!', url: req.url });
 });
 
-app.delete('/auth/*', function(req, res) {
+app.delete('/auth/*', function (req, res) {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  res.json({ success: 'delete call succeed!', url: req.url });
 });
 
-app.listen(3000, function() {
-    console.log("App started")
+app.listen(3000, function () {
+  console.log("App started")
 });
+
+//get a single item from dynamoDB
+function getItem(params) {
+  let promise = new Promise(function (resolve, reject) {
+    dynamodb.get(params, (error, result) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      }
+      if (result.Item) {
+        resolve(result.Item);
+      } else {
+        reject({ error: "item not found" });
+      }
+    });
+  });
+  return promise;
+}
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
